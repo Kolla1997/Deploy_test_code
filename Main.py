1dcd6f582f4e2b94ba83acdf1f0e49cc2d750a5d
dineshkolla <Kolladinesh26@gmail.com>
2025-04-13 16:09:23 -0500

    Testing the rollback commit

--- Main.py
+++ Main.py
@@ -18,7 +18,18 @@ def getRandomInt(min, max):
     """Generate a random integer between min and max"""
     print(random.randint(min, max))
 
+def printProgressbar(iteration, total, prefix='', length=40, fill='█', printEnd='\r'):
+    """Prints a progress bar in the console"""
+    percent = ("{0:.1f}").format(100 * (iteration / float(total)))
+    filledLength = int(length * iteration // total)
+    bar = fill * filledLength + '-' * (length - filledLength)
+    print(f'\r{prefix} |{bar}| {percent}% Complete', end=printEnd)
+    # Print New Line on Complete
+    if iteration == total:
+        print()
+
 if __name__ == "__main__":
     testCall()
     getRandomString(10)
     getRandomInt(1, 10)
+    printProgressbar(5, 10, prefix='Progress', length=50)
